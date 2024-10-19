60a246de417b0ad99c57f658d2995c9ab24126ca
gayatri31prasad <gayatri31prasad@gmail.com>
2024-10-19 14:21:22 +0530

    cors resolve

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const { Users, Categories } = require('./schema/userSchema');
 const express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
-// support parsing of application/json type post data
 app.use(bodyParser.json());
-//support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 const dotenv = require('dotenv');
 dotenv.config();
 
